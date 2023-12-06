574b1f7fcf6b0ed36c8a0e1b9b26718d16b0400f
Aurélien TRONCY <aurelien.troncy@telecomnancy.eu>
2023-12-06 11:56:56 +0100

    [day 05] opti for part2 and new benchmark

--- day05/Part2/part2.py
+++ day05/Part2/part2.py
@@ -2,53 +2,80 @@
 import re
 def part2(lines: list[str]) -> int:
     """Returns the lowest location"""
-    input = "".join(lines)
-    par = input.split("\n\n")
 
-    initial_seeds = [int(x) for x in re.findall(r'\d+', par[0])]
+    initial_seeds = [int(x) for x in re.findall(r'\d+', lines[0])]
     seeds = []
     for i in range(0, len(initial_seeds), 2):
         seeds.append([initial_seeds[i], initial_seeds[i] + initial_seeds[i+1] - 1])
 
-    conv_parts = []
-    for part in par[1:]:
-        conv_dict = {}
-        for line in part.split("\n"):
-            if len(re.findall(r':', line)) != 0:
+    tmp_seeds = []
+    for _, line in enumerate(lines[3:]):
+        numbers = [int(x) for x in re.findall(r'\d+', line)]
+
+        if len(numbers) == 0 and re.findall(r':', line) == []:
+            seeds.extend(tmp_seeds)
+            tmp_seeds = []
+            continue
+        if len(numbers) == 0:
+            continue
+
+        maps = [numbers[0], numbers[1], numbers[1] + numbers[2] - 1]
+    
+        counter = 0
+        while (counter < len(seeds)):
+            seed = seeds[counter]
+            if seed[0] > maps[2] or seed[1] < maps[1]:
+                counter += 1
                 continue
-            numbers = [int(x) for x in re.findall(r'\d+', line)]
+            elif maps[1] <= seed[0] and seed[1] <= maps[2]:
+                tmp_interval = [seed[0], seed[1]]
+                dec = maps[0] - maps[1]
+                interval = [x + dec for x in tmp_interval]
+                tmp_seeds.append(interval)
+                seeds.pop(counter)
+            elif seed[0] <= maps[1] and maps[2] <= seed[1]:
+                if seed[0] == maps[1]:
+                    tmp_interval_mdl = [maps[1], maps[2]]
+                    interval_aft = [maps[2] + 1, seed[1]]
+                    dec = maps[0] - maps[1]
+                    interval_mdl = [x + dec for x in tmp_interval_mdl]
+                    seeds.append(interval_aft)
+                    tmp_seeds.append(interval_mdl)
+                    seeds.pop(counter)
+                elif seed[1] == maps[2]:
+                    tmp_interval_mdl = [maps[1], maps[2]]
+                    interval_pre = [seed[0], maps[1] - 1]
+                    dec = maps[0] - maps[1]
+                    interval_mdl = [x + dec for x in tmp_interval_mdl]
+                    seeds.append(interval_pre)
+                    tmp_seeds.append(interval_mdl)
+                    seeds.pop(counter)
+                else:
+                    tmp_interval_mdl = [maps[1], maps[2]]
+                    interval_pre = [seed[0], maps[1] - 1]
+                    interval_aft = [maps[2] + 1, seed[1]]
+                    dec = maps[0] - maps[1]
+                    interval_mdl = [x + dec for x in tmp_interval_mdl]
+                    seeds.append(interval_pre)
+                    seeds.append(interval_aft)
+                    tmp_seeds.append(interval_mdl)
+                    seeds.pop(counter)
+            elif seed[0] <= maps[1] <= seed[1] <= maps[2]:
+                tmp_interval = [maps[1], seed[1]]
+                interval_pre = [seed[0], maps[1] - 1]
+                dec = maps[0] - maps[1]
+                interval = [x + dec for x in tmp_interval]
+                seeds.append(interval_pre)
+                tmp_seeds.append(interval)
+                seeds.pop(counter)
+            elif maps[1] <= seed[0] <= maps[2] <= seed[1]:
+                tmp_interval = [seed[0], maps[2]]
+                interval_aft = [maps[2] + 1, seed[1]]
+                dec = maps[0] - maps[1]
+                interval = [x + dec for x in tmp_interval]
+                seeds.append(interval_aft)
+                tmp_seeds.append(interval)
+                seeds.pop(counter)
 
-            conv_dict[numbers[1], numbers[1] + numbers[2] - 1] = numbers[0]
-        conv_parts.append(conv_dict)
-
-    for i in range(len(conv_parts)):
-        conv_dict = conv_parts[i]
-        tmp_seeds = []
-        still = seeds
-        while still:
-            seed_start, seed_end = still.pop()
-            added = False
-            for map_start, map_end in conv_dict:
-                dec = conv_dict[(map_start, map_end)]
-                if map_start <= seed_start <= map_end and map_start <= seed_end <= map_end:
-                    tmp_seeds.append((dec + seed_start - map_start, dec + seed_end - map_start))
-                    added = True
-                elif map_start <= seed_end <= map_end:
-                    tmp_seeds.append((dec, dec + seed_end - map_start))
-                    still.append((seed_start, map_start - 1))
-                    added = True
-                elif map_start <= seed_start <= map_end:
-                    tmp_seeds.append((dec + seed_start - map_start, dec + map_end - map_start))
-                    still.append((map_end + 1, seed_end))
-                    added = True
-                elif seed_start < map_start and seed_end > map_end:
-                    tmp_seeds.append((dec, dec + map_end - map_start))
-                    still.append((seed_start, map_start - 1))
-                    still.append((map_end + 1, seed_end))
-                    added = True
-            if not added:
-                tmp_seeds.append((seed_start, seed_end))
-
-        seeds = tmp_seeds
-
+    seeds.extend(tmp_seeds)
     return min(seeds)[0]
