0b291e8fe92761c0f791f3ec97178224262480e1
Aurélien TRONCY <aurelien.troncy@telecomnancy.eu>
2023-12-06 15:45:34 +0100

    [CORE] new launcher with assertions and benchmark over 1000 runs

--- day06/launcher.py
+++ day06/launcher.py
@@ -4,6 +4,7 @@ import platform
 from Part1.part1 import part1
 from Part2.part2 import part2
 
+allowed_options = ['-h', '-p1', '-p2', '-t', '-b', '-a']
 
 def help():
     print("Usage: python3 launcher.py [Options] <filename>")
@@ -11,17 +12,26 @@ def help():
     print("    -h: print this help message")
     print("    -p1: Run only Part 1")
     print("    -p2: Run only Part 2")
-    print("    -t: print the time took to run each part")
+    print("    -t: print the time to run")
     print("    -b: benchmark the code")
+    print("    -a: assert that the script works, use this option with input.txt")
     sys.exit(1)
 
 def main():
     args = sys.argv[1:]
     if len(args) == 0:
         help()
+        sys.exit(0)
+    
+    for arg in args[:-1]:
+        if arg not in allowed_options:
+            print("Error: Invalid options", arg)
+            help()
+            sys.exit(1)
 
     if '-h' in args:
         help()
+        sys.exit(0)
 
     if '-p1' in args and '-p2' in args:
         print("Error: To run both Part 1 and Part 2 use no options")
@@ -29,7 +39,17 @@ def main():
 
     filename = args[-1]
     with open(filename, 'r', encoding='utf-8') as f:
+        if '-b' in args:
+            start_read = time.time()
         lines = f.readlines()
+        if '-b' in args:
+            duration_read = time.time() - start_read
+
+    if '-a' in args:
+        print("Asserting...")
+        print("Part 1:", part1(lines) == 608902)
+        print("Part 2:", part2(lines) == 46173809)
+        sys.exit(0)
 
     if '-b' in args:
         print("Benchmarking...")
@@ -40,16 +60,17 @@ def main():
         print("Day: 06\n")
         print("Part 1:")
         start = time.time()
-        part1(lines)
+        for _ in range(1000):
+            part1(lines)
         time_p1 = time.time() - start
-        print("Time took to run Part 1 :", time_p1)
+        print("Time part 1 (mean time over 1000 runs):", time_p1 / 1000 + duration_read)
         print("Part 2:")
         start = time.time()
-        part2(lines)
+        for _ in range(1000):
+            part2(lines)
         time_p2 = time.time() - start
-        print("Time took to run Part 2 :", time_p2)
-        print("Total time took to run both parts:", time_p1 + time_p2)
-
+        print("Time part 2 (mean time over 1000 runs):", time_p2 / 1000 + duration_read)
+        print("Time part 1 and 2 (mean time over 1000 runs for each part):", time_p1 / 1000 + time_p2 / 1000 + duration_read)
         sys.exit(0)
 
     if '-p1' in args:
@@ -57,26 +78,27 @@ def main():
             start = time.time()
         print("Part 1:", part1(lines))
         if '-t' in args:
-            print("Time took to run Part 1:", time.time() - start)
+            print("Time part 1:", time.time() - start)
         sys.exit(0)
+
     if '-p2' in args:
         if '-t' in args:
             start = time.time()
         print("Part 2:", part2(lines))
         if '-t' in args:
-            print("Time took to run Part 2:", time.time() - start)
+            print("Time part 2:", time.time() - start)
         sys.exit(0)
     
     if '-t' in args:
         start = time.time()
     print("Part 1:", part1(lines))
     if '-t' in args:
-        print("Time took to run Part 1:", time.time() - start)
+        print("Time part 1:", time.time() - start)
     if '-t' in args:
         start = time.time()
     print("Part 2:", part2(lines))
     if '-t' in args:
-        print("Time took to run Part 2:", time.time() - start)
+        print("Time part 2:", time.time() - start)
     sys.exit(0)
     
 
