11dd072e7bec84d78efd675c8e22bfaa2f8b0fad
Aurélien TRONCY <aurelien.troncy@telecomnancy.eu>
2023-12-11 11:03:32 +0100

    [CORE] Change filenames of the launchers template

--- create_launchers.py
+++ create_launchers.py
@@ -3,7 +3,7 @@ import configparser
 
 # Load the config file
 config = configparser.ConfigParser()
-config.read("config.ini")
+config.read("launcher.ini")
 
 # Define the Jinja2 Environment
 env = Environment(
@@ -13,13 +13,12 @@ env = Environment(
 def create_launcher(day_number: str, res_part1: str, res_part2: str) -> None:
     """ Creates a launcher.py file for the given day """
     # Load the template
-    template = env.get_template("template_launcher.jinja2")
+    template = env.get_template("launcher.jinja2")
     # Create the launcher.py file
     with open(f"day{day}/launcher.py", "w") as f:
         f.write(template.render(day_number=day_number, res_part1=res_part1, res_part2=res_part2))
 
 if __name__ == "__main__":
-    # Create the launcher.py files
     for day in config["Days"]:
         res_part1, res_part2 = config["Days"][day].split(",")
         create_launcher(day, res_part1, res_part2)
